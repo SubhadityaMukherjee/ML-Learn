636112806a682c9ca65c416292c5ae73b06adab9
Subhaditya Mukherjee <msubhaditya@gmail.com>
2018-02-06 11:47:41 +0530

    test

--- gitsend.sh
+++ gitsend.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
+echo hello, enter commit
+read comm
 git init
 git add .
-git commit -m "new_comm"
-git push
+git commit -m $comm
+git push https://github.com/SubhadityaMukherjee/ML-Learn.git
+echo uploaded all changes to GitHub  
